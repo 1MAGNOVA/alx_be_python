946cd6b24af8ee0cbf3089d30ded0080e57f6816
1magnova <olalekanabsm@gmail.com>
2024-12-15 11:33:01 -0800

    operations

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,8 +1,8 @@
 number1 = 10 
 number2 = 5
 sums = number1 + number2
-subtraction = number2 - number1
+sub = number2 - number1
 product = number2 * number1
-print("The sum of number1 and number2  is, ", (sums))
-print("The product of number2 and number1 is, ", (product))
-print("The Subtraction of number1 and number2, ", (Subtraction))
+print("Addition of" (number1) " and " (number2 ) "is"(sums))
+print("Subtraction of" (number1) " and " (number2) " is "(sub))
+print("Multiplication of" (number1) " and " (number2) " is " (product))
