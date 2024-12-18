8007cc2b2b80ba8af70080d727ab3c5808cc084a
1magnova <olalekanabsm@gmail.com>
2024-12-18 15:06:44 -0800

    added

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -4,6 +4,6 @@ number2 = 5
 sums = number1 + number2
 sub = number2 - number1
 product = number2 * number1
-print("Addition of" (number1) " and " (number2 ) "is"(sums))
-print("Subtraction of" (number1) " and " (number2) " is "(sub))
-print("Multiplication of" (number1) " and " (number2) " is " (product))
+print(f"Addition of {number1} and {number2} is {addition}.")
+print(f"Subtraction of {number1} and {number2} is {subtraction}.")
+print(f"Multiplication of {number1} and {number2} is {multiplication}.")
