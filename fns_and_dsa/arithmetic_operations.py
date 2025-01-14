ed67f6f076e888a025caa8b2a3461537d79ffb3a
1magnova <olalekanabsm@gmail.com>
2025-01-13 16:12:52 -0800

    operations added

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -2,4 +2,21 @@
 
 #function named perform_operation
 def perform_operation (num1, num2):
-    operation
+    #for add
+    add = lambda num1, num2 : num1 + num2
+    return(num1 + num2)
+
+    #for subtract
+    subtract = lambda num1, num2 : num2 - num1 
+    return(num2 - num1)
+
+    #for multiply
+    multiply = lambda num1, num2: num1 * num2
+    return(num1 * num2)
+
+    #for divide
+    try:
+        divide = lambda num1, num2: num2 / num1
+        return (num2/num1)
+    except ZeroError:
+        print("Number can't divide zero nor be be used to divide zero")
