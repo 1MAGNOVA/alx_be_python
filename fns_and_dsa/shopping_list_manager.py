b3d7af9296185cdcd96d243ce2d41ffaf7d27587
1magnova <olalekanabsm@gmail.com>
2025-01-14 17:49:41 -0800

    shopping list

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Create a Python script named shopping_list_manager.py that implements a simple interface for managing a shopping list.
+This task focuses on using lists to store and manipulate data dynamically.
+"""
+
+#empty list
+shopping_list = [ ]
+
+"""
+Use a loop to continuously display a menu with options to the user until they choose to exit.
+The menu should offer options to add an item, remove an item, view the list, and exit.
+"""
+def shopping_list(choice):
+    for item in shopping_list:
+        if choice == "1":
+            shopping_list[].append()
