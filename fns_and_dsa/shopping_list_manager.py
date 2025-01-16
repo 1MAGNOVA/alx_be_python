203bcef219d76a7e4d30329597824e26bb2ef3ce
1magnova <olalekanabsm@gmail.com>
2025-01-15 17:50:49 -0800

    shopping_list

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -11,18 +11,33 @@ shopping_list = [ ]
 Use a loop to continuously display a menu with options to the user until they choose to exit.
 The menu should offer options to add an item, remove an item, view the list, and exit.
 """
-def shopping_list(choice):
-    for item in shopping_list:
-        if choice == "1":
-            shopping_list[].append(item)
-        elif choice == "2":
-            shopping_list.remove(item)
-        elif choice == "3":
-            return shopping_list
-        elif choice == "4":
-            print("Goodbye!")
-        else:
-            print("Invalid choice. Please try again.")
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
 
- choice = input("Enter your choice: ")
- shopping_list(choice)
+
+
+
+
+def shopping_list(choice):
+  while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        for item in shopping_list:
+            if choice == "1":
+                shopping_list[].append(item)
+             elif choice == "2":
+                shopping_list.remove(item)
+            elif choice == "3":
+                return shopping_list
+            elif choice == "4":
+                print("Goodbye!")
+            else:
+                print("Invalid choice. Please try again.")
+
+choice = input("Enter your choice: ")
+shopping_list(choice)
