93d06531e767ff918c5837511bc45c1083ab3896
1magnova <olalekanabsm@gmail.com>
2025-01-31 18:22:06 -0800

    tem

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -8,7 +8,7 @@ CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 #define convert_to_celsius(fahrenheit) 
 def convert_to_celsius(fahrenheit):
-    return (fahrenheit-32) * CELSIUS_TO_FAHRENHEIT_FACTOR
+    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
 def convert_to_fahrenheit(celsius):
     return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
