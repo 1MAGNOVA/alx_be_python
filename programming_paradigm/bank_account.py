32ee8c1e21bf6347bcea07e7fdd474030565f43f
1magnova <olalekanabsm@gmail.com>
2025-02-09 11:30:17 -0800

    paradigm 1

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -3,13 +3,22 @@ class BankAccount:
     def __init__(self,initial_balance=0.0):
         self.account_balance = initial_balance
 
-    def deposit(amount):
-        if amount >= 1
-            return account_balance + amount
+    def deposit(self,amount):
+        if amount > 0
+            self.account_balance =+ amount
         else: 
-            return "account cannot be - "
+            return "account balance cannot be negative "
 
-    def withdraw(amount):
-        pass
+    def withdraw(self,amount):
+        if amount<= 0:
+            print( "withdrawn amount cannot be negative")
+            return False
+        elif amount > self.account_balance:
+            print ("Insufficient funds")
+            return False
+        else: 
+            self.account_balance =- amount 
+            return True
+
     def display_balance():
-        pass
+        print(f"Current Balance: ${self.account_balance:.2f}")
